2a51f01bcfb24c4e33e70bfec3b065b06c51e925
Sebastian Golian <sebastiangolian@gmail.com>
2019-02-01 13:31:28 +0100

    refactoring project

---
+++ js/app.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const User_js_1 = require("./classes/User.js");
+let user = new User_js_1.User("Jan", "Kowalski");
+user.log();
